059123481a19169ad94d4ae1d532d7342665352b
Tom Scanlan <tscanlan@vmware.com>
2017-03-06 14:42:33 -0500

    update tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
-set -x
+
 set -e
-id=$( docker run  --rm  -d -v "$PWD:/mnt" ansible/ansible:ubuntu1204 sleep 10000 )
 
-docker exec -it $id bash -c 'pip install -U pip;'
+containers="tompscanlan/photon-ansible-base ansible/ansible:fedora24 ansible/ansible:ubuntu1204 ansible/ansible:ubuntu1604 ansible/ansible:opensuse42.2 ansible/ansible:centos7"
 
-docker exec -it $id bash -c '
-	pip install ansible;
-	ln -s /mnt /ansible-role-liota;
-	cd /ansible-role-liota;
-	printf '[defaults]\\\\nroles_path=/:../:.' >ansible.cfg ;
-	ansible-playbook tests/test.yml -i tests/inventory --syntax-check &&
-	ansible-playbook tests/test.yml -i tests/inventory'
-docker kill $id
+cleanup() {
+    echo "Failed tests on $container"
+
+    docker kill $ID
+}
+
+trap cleanup EXIT
+
+for container in $containers; do
+    ID=$( docker run  --rm  -d -v "$PWD:/mnt" $container sleep 10000 )
+
+    docker exec -it $ID bash -c 'pip install -U pip;'
+
+    docker exec -it $ID bash -c '
+        pip install ansible;
+        ln -s /mnt /ansible-role-liota;
+        cd /ansible-role-liota;
+        printf '[defaults]\\\\nroles_path=/:../:.' >ansible.cfg ;
+        ansible-playbook tests/test.yml -i tests/inventory --syntax-check &&
+        ansible-playbook tests/test.yml -i tests/inventory'
+    docker kill $ID
+done
+
+trap - EXIT
+
